610546296836204e4414837e4573645c8347f0cc
Leonardo Falk <leonardo.falk@hotmail.com>
2019-12-05 22:42:30 -0300

    Infinity-Automata

---
+++ infinity-automata/precache-manifest.26fc9fb55bdca225ba2876949cbb22af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818b8b9ddf694bb07a8ae3c6c32e0ff7",
+    "url": "/infinity-automata/index.html"
+  },
+  {
+    "revision": "54c8dfb963b1478a086b",
+    "url": "/infinity-automata/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "59c5e7f0ca4706717a6c",
+    "url": "/infinity-automata/static/css/main.1393cd2c.chunk.css"
+  },
+  {
+    "revision": "54c8dfb963b1478a086b",
+    "url": "/infinity-automata/static/js/2.688e1314.chunk.js"
+  },
+  {
+    "revision": "59c5e7f0ca4706717a6c",
+    "url": "/infinity-automata/static/js/main.26a17d8b.chunk.js"
+  },
+  {
+    "revision": "5b94005431693fc6c758",
+    "url": "/infinity-automata/static/js/runtime-main.bbd5e20e.js"
+  }
+]);
